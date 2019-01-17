eaea23efd06a8d1342df080561155a91b9c795a7
Marcello Semboli <dinogen@gmail.com>
2019-01-17 12:07:33 +0100

    connection parameters as environment variables

--- frontend/config.py
+++ frontend/config.py
@@ -1,11 +1,12 @@
 import os.path
+import os
 SECRET_KEY = b'il mio cuore'
 BASEPATH = os.path.join("/opt","voting")
 DBTYPE="postgresql"
 DBPATH = os.path.join(BASEPATH, "database","copernico-spritz.sqlite3.db")
-DBHOST = "localhost"
-DBNAME = "spritz"
-DBUSER = "dinogen"
-DBPWD = "abc123"
+DBHOST = os.environ.get('POSTGRES_HOST')   # "localhost"
+DBNAME = os.environ.get('POSTGRES_DBNAME') # "spritz"
+DBUSER = os.environ.get('POSTGRES_USER')   # "dinogen"
+DBPWD  = os.environ.get('POSTGRES_PWD')    # "abc123"
 BINPATH = os.path.join(BASEPATH, "bin")
 ELECTIONPATH = os.path.join(BASEPATH,"elections")
