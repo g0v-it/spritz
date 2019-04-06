32ed180e82bb6d395c11b449a151c5ac74c923db
Marcello Semboli <dinogen@gmail.com>
2019-04-06 15:57:34 +0200

    tests fixed!

--- frontend/user_test.py
+++ frontend/user_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 import user
-import auth
+import auth_test as auth
 
 class user_test(unittest.TestCase):
     def setUp(self):
@@ -26,6 +26,7 @@ class user_test(unittest.TestCase):
         user.delete_user_by_username( u.user_name)
         user.insert_user_dto(u)
         user.delete_user_by_username( u.user_name)
+        auth.ADD_UNKNOWN_USER = False
         u1 = user.load_user_by_username(u.user_name)
         self.assertIsNone( u1 )
         
@@ -51,6 +52,7 @@ class user_test(unittest.TestCase):
         self.assertEqual(u.user_name, u1.user_name)
         self.assertEqual(u.pass_word, u1.pass_word)
         user.delete_user_by_id(u.user_id)
+
     def test_load_by_id(self):
         self.assertIsNone(user.load_user_by_id(999))
         
--- frontend/vote_test.py
+++ frontend/vote_test.py
@@ -2,6 +2,7 @@
 import unittest
 import vote
 import votation
+import vote_maj_jud
 
 class vote_test(unittest.TestCase):
     def test_insert(self):
@@ -25,57 +26,57 @@ class vote_test(unittest.TestCase):
         self.assertEqual(0,len(ar))
 
     def test_median_calc3(self):
-        self.assertEqual(1,vote.maj_jud_median_calc([1,1,1]))
-        self.assertEqual(0,vote.maj_jud_median_calc([4,2,1]))
-        self.assertEqual(1,vote.maj_jud_median_calc([5,10,0]))
-        self.assertEqual(0,vote.maj_jud_median_calc([0,0,0]))
-        self.assertEqual(2,vote.maj_jud_median_calc([10,0,20]))
-        self.assertEqual(1,vote.maj_jud_median_calc([123,567,456]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([1,1,1]))
+        self.assertEqual(0,vote_maj_jud.maj_jud_median_calc([4,2,1]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([5,10,0]))
+        self.assertEqual(0,vote_maj_jud.maj_jud_median_calc([0,0,0]))
+        self.assertEqual(2,vote_maj_jud.maj_jud_median_calc([10,0,20]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([123,567,456]))
     def test_median_calc4(self):
-        self.assertEqual(1,vote.maj_jud_median_calc([1,1,1,1]))
-        self.assertEqual(0,vote.maj_jud_median_calc([7,2,1,1]))
-        self.assertEqual(1,vote.maj_jud_median_calc([5,10,0,4]))
-        self.assertEqual(0,vote.maj_jud_median_calc([0,0,0,0]))
-        self.assertEqual(2,vote.maj_jud_median_calc([10,0,20,8]))
-        self.assertEqual(1,vote.maj_jud_median_calc([123,567,456,34]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([1,1,1,1]))
+        self.assertEqual(0,vote_maj_jud.maj_jud_median_calc([7,2,1,1]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([5,10,0,4]))
+        self.assertEqual(0,vote_maj_jud.maj_jud_median_calc([0,0,0,0]))
+        self.assertEqual(2,vote_maj_jud.maj_jud_median_calc([10,0,20,8]))
+        self.assertEqual(1,vote_maj_jud.maj_jud_median_calc([123,567,456,34]))
 
     def test_compare(self):
         ar1 = [1,1,1]
         ar2 = [1,1,1]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  0)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  0)
         ar1 = [1,1,3]
         ar2 = [1,1,1]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  +1)
-        self.assertEqual( vote.maj_jud_compare(ar2,ar1),  -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar2,ar1),  -1)
         ar1 = [2,6,2] 
         ar2 = [3,4,3]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  +1)
         ar1 = [20,6,2] 
         ar2 = [3,4,30]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  -1)
         ar1 = [0,0,20] 
         ar2 = [0,10,0]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  +1)
         ar1 = [10,10,10] 
         ar2 = [10,11, 9]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  +1)
         ar1 = [5,10,5] 
         ar2 = [3,10, 7]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  -1)
         ar1 = [1000,1000,1000] 
         ar2 = [1000,1000,1000]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  0)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  0)
         ar1 = [1000,1000,1000] 
         ar2 = [950,960,1090]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  -1)
         ar1 = [0,0,1000] 
         ar2 = [0,0,900]
-        self.assertEqual( vote.maj_jud_compare(ar1,ar2),  +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(ar1,ar2),  +1)
     def test_sort(self):
-        r1 = vote.maj_jud_result(1,[100,100,100,100])
-        r2 = vote.maj_jud_result(2,[50,100,150,100])
-        r3 = vote.maj_jud_result(3,[100,150,100,50])
-        r4 = vote.maj_jud_result(4,[50,100,100,150])
+        r1 = vote_maj_jud.maj_jud_result(1,[100,100,100,100])
+        r2 = vote_maj_jud.maj_jud_result(2,[50,100,150,100])
+        r3 = vote_maj_jud.maj_jud_result(3,[100,150,100,50])
+        r4 = vote_maj_jud.maj_jud_result(4,[50,100,100,150])
         a = sorted([r1,r2,r3,r4])
         b = a[::-1] # reverse the list
         self.assertEqual(4,b[0].option_id)
@@ -95,20 +96,20 @@ class vote_test(unittest.TestCase):
             u.vote_key = "vote_key2_" + str(i)
             u.jud_value = 3
             vote.insert_dto(u)
-        self.assertEqual([0,10,0,20,0,0], vote.count_votes_by_option(999,1001))
+        self.assertEqual([0,10,0,20,0,0], vote_maj_jud.count_votes_by_option(999,1001))
     def test_mio(self):
         b = [22,19,19,15,19,14]
         r = [24,17,18,18,10,21]
-        self.assertEqual( vote.maj_jud_compare(b,r), -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(b,r), -1)
         b = [100,100,100,100,100,100]
         r = [100,100,100,100,100,100]
-        self.assertEqual( vote.maj_jud_compare(b,r), 0)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(b,r), 0)
         b = [100,100, 99,101,100,100]
         r = [100,100,100,100,100,100]
-        self.assertEqual( vote.maj_jud_compare(b,r), +1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(b,r), +1)
         b = [100,100,100,100,100,100]
         r = [100,100,100,100, 99,101]
-        self.assertEqual( vote.maj_jud_compare(b,r), -1)
+        self.assertEqual( vote_maj_jud.maj_jud_compare(b,r), -1)
 if __name__ == '__main__':
     unittest.main()
 
